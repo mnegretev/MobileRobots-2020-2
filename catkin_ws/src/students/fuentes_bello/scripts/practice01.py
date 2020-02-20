726a2c381c9268c5b44fc1f2715e8030c5c8c1eb
greenreader13 <david_1393@comunidad.unam.mx>
2020-02-20 16:46:42 -0600

    Primera modificación

--- catkin_ws/src/students/fuentes_bello/scripts/practice01.py
+++ catkin_ws/src/students/fuentes_bello/scripts/practice01.py
@@ -12,7 +12,7 @@ import rospy
 import tf
 from std_msgs.msg import Float32MultiArray
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "Fuentes Bello David"
 
 def get_robot_pose(listener):
     try:
