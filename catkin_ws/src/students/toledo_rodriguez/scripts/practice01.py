e06b24919a1273c0274d84307ae37e972684a07a
Aketzally Ollin Toledo Rodríguez <aketzally.toledor@gmail.com>
2020-02-26 18:54:07 -0600

    Actualizar nombre

--- catkin_ws/src/students/toledo_rodriguez/scripts/practice01.py
+++ catkin_ws/src/students/toledo_rodriguez/scripts/practice01.py
@@ -12,7 +12,7 @@ import rospy
 import tf
 from std_msgs.msg import Float32MultiArray
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "TOLEDO_RODRIGUEZ"
 
 def get_robot_pose(listener):
     try:
