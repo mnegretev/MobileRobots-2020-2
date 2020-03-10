b26f24cae396888e3fa88b7f23da47362143772e
JSantiagoS <juan.ss1992@gmail.com>
2020-03-10 13:51:30 -0600

    practica1

--- catkin_ws/src/students/santiago_santiago/scripts/practice01.py
+++ catkin_ws/src/students/santiago_santiago/scripts/practice01.py
@@ -10,9 +10,12 @@
 
 import rospy
 import tf
+import time
 from std_msgs.msg import Float32MultiArray
 
-NAME = "Santiago Santiago"
+speed = Float32MultiArray()
+
+NAME = "santiago santiago"
 
 def get_robot_pose(listener):
     try:
@@ -28,23 +31,24 @@ def get_robot_pose(listener):
     return None
 
 def main():
+    global speed
     print "PRACTICE 01 - " + NAME
     rospy.init_node("practice01")
     pub_speeds = rospy.Publisher("/rotombot/hardware/motor_speeds", Float32MultiArray, queue_size=10)
     loop = rospy.Rate(20)
     listener = tf.TransformListener()
 
+    speed.data = [0.0, 0.0]
+
     while not rospy.is_shutdown():
-        #
-        # TODO:
-        # Declare a Float32MultiArray message and assign the appropiate speeds:
-        # [sl, sr] where sl is the left tire speed and sr, the right tire speed, both in m/s
-        # Calculate the speeds to move the robot describing a 2mx2m square.
-        # You can do it in open or closed loop. For the latter case, you can use the
-        # get_robot_pose function to get the current robot configuration.
-        # Publish the message.
-        # You can declare as many variables as you need.
-        #
+	
+	speed.data = [1.0, 1.0]
+	pub_speeds.publish(speed)
+	time.sleep(2)
+	speed.data = [1.0, -1.0]
+	pub_speeds.publish(speed)
+	time.sleep(0.35)
+	
         loop.sleep()
 
 
