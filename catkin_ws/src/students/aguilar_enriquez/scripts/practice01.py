3b1f94a99f915b1047932a4afa691ffe6217fee6
penserbjorne <paul.aguilar.enriquez@hotmail.com>
2020-03-24 15:53:50 -0600

    Se añadio la practica1

--- catkin_ws/src/students/aguilar_enriquez/scripts/practice01.py
+++ catkin_ws/src/students/aguilar_enriquez/scripts/practice01.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 # AUTONOMOUS MOBILE ROBOTS - UNAM, FI, 2020-2
-# PRACTICE 1 - THE PLATFORM ROS 
+# PRACTICE 1 - THE PLATFORM ROS
 #
 # Instructions:
 # Write a program to move the robot along a path describing a 2mx2m square.
@@ -12,7 +12,21 @@ import rospy
 import tf
 from std_msgs.msg import Float32MultiArray
 
-NAME = "Aguilar_Enriquez"
+NAME = "Aguilar Enriquez"
+
+# Funcion para mover
+def move(steps, sl, sr, pub_speeds, loop):
+    msg = Float32MultiArray()
+    while not rospy.is_shutdown() and steps:
+        msg.data = [sl,sr]
+        pub_speeds.publish(msg)
+        steps = steps - 1
+        loop.sleep()
+
+    msg.data = [0,0]
+    pub_speeds.publish(msg)
+
+    return
 
 def get_robot_pose(listener):
     try:
@@ -33,25 +47,36 @@ def main():
     pub_speeds = rospy.Publisher("/rotombot/hardware/motor_speeds", Float32MultiArray, queue_size=10)
     loop = rospy.Rate(20)
     listener = tf.TransformListener()
+#
+# TODO:
+# Declare a Float32MultiArray message and assign the appropiate speeds:
+# [sl, sr] where sl is the left tire speed and sr, the right tire speed, both in m/s
+# Calculate the speeds to move the robot describing a 2mx2m square.
+# You can do it in open or closed loop. For the latter case, you can use the
+# get_robot_pose function to get the current robot configuration.
+# Publish the message.
+# You can declare as many variables as you need.
+#
+    # Sabemos que un metro implica 40 pasitos
+    # Asi que queremos recorrer dos metros
+    steps = 80
+    # Sabemos que para girar 45 grados necesitamos 16 pasos
+    turn = 16
+    # Dejamos las velocidades que ya conocemos para la velocidad
+    sl = 0.5
+    sr = 0.5
 
-    while not rospy.is_shutdown():
-        #
-        # TODO:
-        # Declare a Float32MultiArray message and assign the appropiate speeds:
-        # [sl, sr] where sl is the left tire speed and sr, the right tire speed, both in m/s
-        # Calculate the speeds to move the robot describing a 2mx2m square.
-        # You can do it in open or closed loop. For the latter case, you can use the
-        # get_robot_pose function to get the current robot configuration.
-        # Publish the message.
-        # You can declare as many variables as you need.
-        #
-        loop.sleep()
-
+    # Necesitamos avanzar y girar, esto se hace por cada lado
+    # por lo cual necesitamos hacerlo cuatro veces
+    for i in range(4):
+        # Avanzamos dos metros
+        move(steps, sl, sr, pub_speeds, loop)
+        # Giramos
+        # Para el giro solo hay que cambiar la velocidad de una llanta
+        move(turn, -sl, sr, pub_speeds, loop)
 
 if __name__ == '__main__':
     try:
         main()
     except rospy.ROSInterruptException:
         pass
-    
-
